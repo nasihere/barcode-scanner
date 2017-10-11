3989bab3b5b3353b3f2de6beb8edd4f53bc6b62d
Sayed, Nasir (CORP) <Nasir.Sayed@adp.com>
2017-10-10 21:24:06 -0400

    port added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 // Define the port to run on
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 app.set('port', PORT);
 app.use(express.static(__dirname + '/www'));
 // Listen for requests
