bc5de0f65d0d315709a24dbdaf3115570facc34a
Sayed, Nasir (CORP) <Nasir.Sayed@adp.com>
2017-10-10 20:49:57 -0400

    heroku start command added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Camera } from '@ionic-native/camera';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BarcodeScanner ,BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
+  scanData : {};
+  encodeData : string ;
+  encodedData : {} ;
+  options :BarcodeScannerOptions;
+  data = [
+        {
+            barcode: "2938989289289",
+            active: "Inactive",
+            dateTime: '01/02/2017',
+            timesScanned: "11",
+            info: 'something something something something something' 
+        },
+        {
+            barcode: "393893898998",
+            active: "Active",
+            dateTime: '02/03/2017',
+            timesScanned: "2",
+            info: 'Doing it something Doing it' 
+        },
+    ]
+    constructor(private barcodeScanner: BarcodeScanner,public navCtrl: NavController) {
+        this.scan();
+    }
+    getData() {
+        return this.data;
+    }
+    scan() {
+        this.options = {
+            prompt : "Scan your barcode "
+        }
+        this.barcodeScanner.scan(this.options).then((barcodeData) => {
+            console.log(barcodeData);
+            this.scanData = barcodeData;
+        }, (err) => {
+            console.log("Error occured : " + err);
+        });         
+    }
 
 }
